ea40a27eee6b86b71fbec0c6b75f7e6732d2f7f3
xogus6510 <xogus6510@kakao.com>
2022-08-01 18:31:18 +0900

    08-01 회사 마지막 커밋

--- all.js
+++ all.js
@@ -3,19 +3,18 @@ const All = {
   <h1>all2</h1>
   <ul class="list">
       <li v-for="todoItem, index) in todoItems" :key="todoItem">
-           <p>{{ todoItem}}</p>
+           <p>{{index}}-{{ todoItem}}</p>
       </li>
   </ul>`,
     data() {
-        return {todoItems: []};
+        return {todoItems: [], todoValue: []};
     },
     created: function() {
         if (localStorage.length > 0) {
             for (let i = 0; i < localStorage.length; i++) {
                 // 로컬스토리지의 key를 이용하여 value를 가져옴 로컬스토리지에 저장된 값은 string이므로 JavaScript에 맞는 적절한 형태로
                 // 변환하기 위해 JSON.parse() API 사용
-              this.todoItems.push(localStorage.key(i));
-               this.todoItems.push(localStorage.key(i));
+              this.todoItems.push(localStorage.key(i), localStorage.getItem(localStorage.key(i)));
               this.todoItems.sort();
             }
         }
