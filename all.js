e27ebbfd89a30506354b7b25857a889be85c1e46
xogus6510 <xogus6510@kakao.com>
2022-08-01 14:54:57 +0900

    로컬스토리지 불러오기(실시간x)

--- all.js
+++ all.js
@@ -1,3 +1,26 @@
 const All = {
-                template: `<h1>all</h1>`
-            };
+  template: `
+  <h1>all2</h1>
+  <ul class="list">
+      <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+          <input type="checkbox" v-bind:id="todoItem.item" />
+          <label v-bind:for="todoItem.item" class="list__label">
+               <p>{{ todoItem }}</p>
+          </label>
+      </li>
+  </ul>`,
+  data() {
+    return {
+      todoItems: []
+    };
+  },
+  created() {
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+          this.todoItems.push(localStorage.getItem(localStorage.key(i)));
+        }
+      }
+    }
+  }
+};
