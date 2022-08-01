335c2addacdfe8bc6b830ed08e0b753a71d96825
xogus6510 <xogus6510@kakao.com>
2022-08-01 18:19:15 +0900

    로컬스토리지 진행중

--- all.js
+++ all.js
@@ -1,19 +1,23 @@
 const All = {
-  template: `
+    template: `
   <h1>all2</h1>
   <ul class="list">
-      <li v-for="todoItem in todoItems" :key="todoItem">
+      <li v-for="todoItem, index) in todoItems" :key="todoItem">
            <p>{{ todoItem}}</p>
       </li>
   </ul>`,
-  data() {
-    return {
-      todoItems: []
-    };
-  },
-  created() {
-      for (let i = 0; i < localStorage.length; i++) { 
-          this.todoItems.push(localStorage.getItem(localStorage.key(i)));
+    data() {
+        return {todoItems: []};
+    },
+    created: function() {
+        if (localStorage.length > 0) {
+            for (let i = 0; i < localStorage.length; i++) {
+                // 로컬스토리지의 key를 이용하여 value를 가져옴 로컬스토리지에 저장된 값은 string이므로 JavaScript에 맞는 적절한 형태로
+                // 변환하기 위해 JSON.parse() API 사용
+              this.todoItems.push(localStorage.key(i));
+               this.todoItems.push(localStorage.key(i));
+              this.todoItems.sort();
+            }
         }
     }
 };
