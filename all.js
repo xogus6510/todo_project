8a99c46f97b37ffaae98eca67b3bf0ea558bb059
park taehyun <xogus6510@kakao.com>
2022-08-02 23:16:53 +0900

    오류 제거

--- all.js
+++ all.js
@@ -8,25 +8,21 @@ const All = {
       </li>
   </ul>`,
     data() {
-        return {todoItems: [], todoValue: []};
+        return {todoItems: []};
     },
     methods: {
         deleteTodoItem: function(todoItem, index) {
             localStorage.removeItem(todoItem.id);
             this.todoItems.splice(index, 1);
         }
-    },
+    }, props: ['propsdata'],
     created: function() {
         if (localStorage.length > 0) {
             for (let i = 0; i < localStorage.length; i++) {
-                // 로컬스토리지의 key를 이용하여 value를 가져옴 로컬스토리지에 저장된 값은 string이므로 JavaScript에 맞는 적절한 형태로
-                // 변환하기 위해 JSON.parse() API 사용
-              this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-                //this.todoItems.sort();
-                 this.todoItems.sort(function(a,b){
+                this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+                this.todoItems.sort(function(a,b){
                 return a.id - b.id;
               });
-              console.log(this.todoItems);
             }
         }
     }
