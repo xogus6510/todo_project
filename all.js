68ab9275c5bf900c70c11d9d106bc3641aca1ff6
xogus6510 <xogus6510@kakao.com>
2022-08-02 18:37:18 +0900

    최종 커밋

--- all.js
+++ all.js
@@ -2,13 +2,19 @@ const All = {
     template: `
   <h1>all2</h1>
   <ul class="list">
-      <li v-for="todoItem, index) in todoItems" :key="todoItem">
-           <p>{{index}}번 {{ todoItem.item}} - {{todoItem.date}}</p>
-         
+      <li v-for="(todoItem, index) in todoItems" :key="todoItem">
+           <p>{{ todoItem.id}} - {{ todoItem.item}} - {{todoItem.date}}</p>
+              <button @click="deleteTodoItem(todoItem, index)">Delete</button>
       </li>
   </ul>`,
     data() {
         return {todoItems: [], todoValue: []};
+    },
+    methods: {
+        deleteTodoItem: function(todoItem, index) {
+            localStorage.removeItem(todoItem.id);
+            this.todoItems.splice(index, 1);
+        }
     },
     created: function() {
         if (localStorage.length > 0) {
@@ -16,7 +22,10 @@ const All = {
                 // 로컬스토리지의 key를 이용하여 value를 가져옴 로컬스토리지에 저장된 값은 string이므로 JavaScript에 맞는 적절한 형태로
                 // 변환하기 위해 JSON.parse() API 사용
               this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-              this.todoItems.sort();
+                //this.todoItems.sort();
+                 this.todoItems.sort(function(a,b){
+                return a.id - b.id;
+              });
               console.log(this.todoItems);
             }
         }
