854fa9fc53081f69afea4eb1e9061d7f3db1ad15
xogus6510 <xogus6510@kakao.com>
2022-08-03 10:32:03 +0900

    0803 오전

--- todo2/todolist.js
+++ todo2/todolist.js
@@ -4,14 +4,17 @@ const Todolist = {
     <ul>
         <li v-for="(todoItem, index) in propsdata" :key="todoItem">
             {{ todoItem }}
-            <button type="button" v-on:click="removeTodo(todoItem, index)">삭제</button>
+            <button type="button" v-on:click="removetodo(todoItem, index)">삭제</button>
         </li>
     </ul>
 </section>
   `, props: ['propsdata'],
   methods: {
-      removeTodo(todoItem, index){
-          this.$emit('removeTodo', todoItem, index);
+      removetodo(todoItem, index) {
+            console.log('2번' + todoItem + index);
+            this.$emit('removetodo');
+           // localStorage.removeItem(todoItem);
+                    
       }
   } 
 };
