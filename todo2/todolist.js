571387a7b761de9583051ca0c8f4a80bc3be58d8
xogus6510 <xogus6510@kakao.com>
2022-08-04 18:14:18 +0900

    수정버튼생성

--- todo2/todolist.js
+++ todo2/todolist.js
@@ -5,16 +5,21 @@ const Todolist = {
         <li v-for="todoItem in propsdata" :key="todoItem.id">
             
             등록시간 : {{todoItem.date}} / {{todoItem.item}}
-            <button type="button" v-on:click="removetodo(todoItem, index)">삭제</button>
+            <button type="button" v-on:click="removetodo(todoItem)">삭제</button>
+            <button type="button" v-on:click="edittodo(todoItem)">수정</button>
         </li>
     </ul>
 </section>
   `, props: ['propsdata'],
   methods: {
-      removetodo(todoItem, index) {
+      removetodo(todoItem) {
             console.log('삭제클릭 emit, ' + todoItem + "=id");
             this.$emit('removetodo', todoItem);
                     
+      },
+      edittodo(todoItem) {
+            console.log('수정클릭 emit, ' + todoItem + "=id");
+            this.$emit('edittodo', todoItem);
       }
   } 
 };
